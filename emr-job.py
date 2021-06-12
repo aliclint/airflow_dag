11e7688ef77a887b1db8c350cd079a4ea56258ca
Clinton Ali <aliclinton@gmail.com>
2021-06-12 07:45:22 -0400

    Add files via upload

---
+++ emr-job.py
@@ -0,0 +1,79 @@
+import airflow
+
+DEFAULT_ARGS = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'start_date': airflow.utils.dates.days_ago(0),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'sla_miss_callback': yet_another_function,
+    # 'trigger_rule': 'all_success'
+}
+
+CLUSTER_ID= 'j-ZYDTUXUVO4XL'
+
+SPARK_STEPS = [
+    {
+        'Name': 'wcd_midterm_data_processing',
+        'ActionOnFailure': 'CONTINUE',
+        'HadoopJarStep': {
+            'Jar': 'command-runner.jar',
+            'Args': [
+                '/usr/bin/spark-submit',
+                '--class', 'Driver.MainApp',
+                '--master', 'yarn',
+                '--deploy-mode', 'cluster',
+                '--num-executors', '2',
+                '--diver-memory','1g',
+                '--executors-cores','2',
+                's3://wcd-midterm-1-demo/wcd_final_project_2.11-0.1.jar',
+                '-p','wcd-midterm-1-demo',
+                '-i','Csv',
+                '-o','parquet',
+                '-s','s3://wcd-midterm-1-demo/airport.csv',
+                '-d','s3://wcd-midterm-1-demo/data',
+                '-c','job',
+                '-m','append',
+                '--input_options', 'header=true'
+                ],
+        },
+    }
+]
+
+dag = DAG(
+    dag_id'emr_job_flow_manual_steps_dag',
+    default_args=DEFAULT_ARGS,
+    dagrun_timeout=timedelta(hours=2)
+    tags=['emr']
+)
+
+ step_adder = EmrAddStepsOperator(
+        task_id='add_steps',
+        job_flow_id="{{ task_instance.xcom_pull(task_ids='create_job_flow', key='return_value') }}",
+        aws_conn_id='aws_default',
+        steps=SPARK_STEPS,
+        dag=dag
+    )
+
+step_checker = EmrStepSensor(
+    task_id='watch_step',
+    job_flow_id=CLUSTER_ID,
+    step_id="{{ task_instance.xcom_pull(task_ids='add_steps', key='return_value')[0] }}",
+    aws_conn_id='aws_default',
+)
+
+step_checker.step_upstream(step_adder)
