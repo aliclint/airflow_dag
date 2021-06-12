f48ae60deba105bf21b2aac89db85f865b64f5f0
Clinton Ali <aliclinton@gmail.com>
2021-06-12 07:53:16 -0400

    Add files via upload

--- emr-job.py
+++ emr-job.py
@@ -1,5 +1,15 @@
 import airflow
 
+from datetime import timedelta
+from textwrap import dedent
+
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+
+# Operators; we need this to operate!
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
 DEFAULT_ARGS = {
     'owner': 'airflow',
     'depends_on_past': False,
